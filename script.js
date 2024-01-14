4ebb848434dfa260d12c1d90e4d842e629896d0b
Sejal207 <145064862+Sejal207@users.noreply.github.com>
2024-01-15 00:22:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box")
+const listcont = document.getElementById("list_cont")
+
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("Please write something")
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listcont.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value='';
+    saveData();
+}
+
+listcont.addEventListener("click",function(e){
+    if (e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listcont.innerHTML);
+}
+function showTask(){
+    listcont.innerHTML = localStorage.getItem("data");
+}
+showTask();
